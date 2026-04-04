94fab3fc9256d2e90b07ca496271a87a2fd8011b
gustofied <gustofied@gmail.com>
2026-04-04 16:58:49 +0200

    lr

--- exploration/np5.py
+++ exploration/np5.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+import numpy as np
 
 dir = Path(__file__).resolve().parent
 data_path = dir / "data"/ "clean_weather.csv"
@@ -35,3 +36,42 @@ y_pred = [2.5, 0.0, 2, 8]
 mse = mean_squared_error(y_true, y_pred)
 
 print("Mean Squared Error:", mse)
+
+loss = lambda w, y: ((w * 80 + 11.99) - y) ** 2
+gradient = lambda w, y: ((w * 80 + 11.99) - y) * 2
+y = 81
+
+ws = np.arange(-1, 3, .05)
+
+losses = loss(ws, y)
+gradients = gradient(ws, y)
+
+
+# plt.scatter(ws, losses)
+plt.scatter(ws, gradients)
+
+plt.scatter
+plt.plot(1.25, gradient(1.25, y), 'ro')
+plt.show()
+
+
+ws = np.arange(-4000, 100, 100)
+losses = loss(ws , y)
+
+plt.scatter(ws, losses)
+plt.plot(1, loss(1, y), 'ro')
+new_weight = 1 - gradient(1, y) * 80
+plt.plot(new_weight, loss(new_weight, y), 'go')
+plt.show()
+
+
+ws = np.arange(-.5, 1.5, .1)
+losses = loss(ws, y)
+
+plt.scatter(ws, losses)
+
+plt.plot(1, loss(1, y), 'ro')
+lr = 2e-5
+new_weight = 1 - lr * gradient(1, y) * 80
+plt.plot(new_weight, loss(new_weight, y), 'go')
+plt.show()
